01a1695faf5b28654e1544b6079ad11a331897e3
AlexanderBadger <sashazateev444@gmail.com>
2023-08-01 20:58:58 +0200

    added 1 2 3 tasks

---
+++ task002/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("insert number");
+string daynumber = Console.ReadLine();
+
+if (daynumber == "1")
+{
+    Console.WriteLine("Пон");
+}
+else if (daynumber == "2")
+{
+    Console.WriteLine("вт");
+}
+else if (daynumber == "3")
+{
+    Console.WriteLine("ср");
+}
+else if (daynumber == "4")
+{
+    Console.WriteLine("чт");
+}
+else if (daynumber == "5")
+{
+    Console.WriteLine("пт");
+}
+else if (daynumber == "6")
+{
+    Console.WriteLine("сб");
+}
+else if (daynumber == "7")
+{
+    Console.WriteLine("вс");
+}
+else
+{
+    Console.WriteLine("fail");
+}
---
+++ task003/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("insertNumidk");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    number = -number;
+}
+int count = -number;
+while (count <= number)
+{
+    Console.Write($"{count}, ");
+    count++;
+}
