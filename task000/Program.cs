67fbd845eeaf65d7ad9810a9f12efc96ecccf8f6
AlexanderBadger <sashazateev444@gmail.com>
2023-08-01 20:06:41 +0200

    task000 finished

--- task000/Program.cs
+++ task000/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("insert number");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("i smell like concentrated tree bark!!");
+x = x * x;
+Console.WriteLine($"^2 ={x}");
